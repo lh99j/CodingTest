30c12fc54e85528c68144c7e4a9f4f65d1810028
lh99j <grand2181@gmail.com>
2023-01-06 13:18:13 +0900

    백준 2941(s5) - 크로아티아 알파벳

--- CodingTest/src/main/kotlin/Backjoon/silver/2941.kt
+++ CodingTest/src/main/kotlin/Backjoon/silver/2941.kt
@@ -5,19 +5,15 @@ fun main(args: Array<String>) {
 
     for (i in croatia.indices) {
         while (str.contains(croatia[i])) {
-            println(str)
             if(str.indexOf(croatia[i]) == 0){
                 str = " " + str.substring(str.indexOf(croatia[i]) + croatia[i].length, str.length)
-                println("0-0")
             }else if(str.indexOf(croatia[i]) == 1){
                 str = str.substring(0, 1) + " " + str.substring(str.indexOf(croatia[i]) + croatia[i].length, str.length)
             }
             else{
                 str = str.substring(0, str.indexOf(croatia[i]) - 1) + " " + str.substring(str.indexOf(croatia[i]) + croatia[i].length, str.length)
             }
-
             answer++
-            println(str)
         }
     }
 
