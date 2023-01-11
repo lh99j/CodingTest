5e058d11f20a02df30d44db82e43ba6fd6fa6a59
lh99j <grand2181@gmail.com>
2023-01-11 15:16:58 +0900

    백준 10757

--- CodingTest/src/main/kotlin/Backjoon/silver/practice.kt
+++ CodingTest/src/main/kotlin/Backjoon/silver/practice.kt
@@ -3,24 +3,7 @@ import java.lang.StringBuilder
 
 fun main(args: Array<String>) {
     var br = BufferedReader(InputStreamReader(System.`in`))
-    var num = br.readLine().toInt()
+    var (a, b) = br.readLine().split(" ").map { it.toBigInteger()}
 
-    for(i in 0 until num){
-        var a = br.readLine().toInt()
-        var b = br.readLine().toInt()
-
-        a++
-
-        println(fibo(a, b))
-    }
-
-}
-
-fun fibo(a: Int, b: Int): Int{
-    return if(b == 1)
-        1
-    else if(a == 1)
-        b
-    else
-        fibo(a, b - 1) + fibo(a - 1, b)
+    println(a + b)
 }
