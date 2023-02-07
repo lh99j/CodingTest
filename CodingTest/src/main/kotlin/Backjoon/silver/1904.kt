af68a2f36889d900bbfcdcd92f45cbcb8cd550d3
lh99j <grand2181@gmail.com>
2023-02-07 13:44:30 +0900

    백준 1904(s3) - 01타일 코드 수정

--- CodingTest/src/main/kotlin/Backjoon/silver/1904.kt
+++ CodingTest/src/main/kotlin/Backjoon/silver/1904.kt
@@ -1,19 +1,38 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
+//초기버전(시간초과)
+
+//fun main(args: Array<String>) {
+//    val br = BufferedReader(InputStreamReader(System.`in`))
+//    val num = br.readLine().toInt()
+//
+//    println(tileRecursion(num) % 15746)
+//
+//}
+//
+//fun tileRecursion(num: Int): Int{
+//    if(num == 1)
+//        return 1
+//    else if(num == 2)
+//        return 2
+//    else
+//        return tileRecursion(num - 2) + tileRecursion(num - 1)
+//}
+
+
 fun main(args: Array<String>) {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val num = br.readLine().toInt()
+    var array = mutableListOf<Int>()
 
-    println(tileRecursion(num))
+    array.add(1)
+    array.add(2)
 
-}
+    for(i in 2..num){
+        val tempNum = array[i - 2] + array[i - 1]
+        array.add(tempNum % 15746)
+    }
 
-fun tileRecursion(num: Int): Int{
-    if(num == 1)
-        return 1
-    else if(num == 2)
-        return 2
-    else
-        return tileRecursion(num - 2) + tileRecursion(num - 1)
+    println(array[num - 1])
 }
