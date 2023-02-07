3891e15594f8308b42ebe1ece4809fdcd8cee792
lh99j <grand2181@gmail.com>
2023-02-07 14:13:56 +0900

    no message

--- CodingTest/src/main/kotlin/Backjoon/silver/practice.kt
+++ CodingTest/src/main/kotlin/Backjoon/silver/practice.kt
@@ -1,9 +1,3 @@
-import java.io.*
-import java.lang.StringBuilder
+fun main(args: Array<String>){
 
-fun main(args: Array<String>) {
-    var br = BufferedReader(InputStreamReader(System.`in`))
-    var (a, b) = br.readLine().split(" ").map { it.toBigInteger()}
-
-    println(a + b)
 }
