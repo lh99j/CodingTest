658b817d8a21d1ee82554e63edd08f1bd6c83b51
lh99j <grand2181@gmail.com>
2023-02-09 13:30:49 +0900

    백준 1904(S3) - 01타일

--- CodingTest/src/main/kotlin/Backjoon/silver/1904.kt
+++ CodingTest/src/main/kotlin/Backjoon/silver/1904.kt
@@ -2,37 +2,42 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 //초기버전(시간초과)
-
-//fun main(args: Array<String>) {
-//    val br = BufferedReader(InputStreamReader(System.`in`))
-//    val num = br.readLine().toInt()
-//
-//    println(tileRecursion(num) % 15746)
-//
-//}
-//
-//fun tileRecursion(num: Int): Int{
-//    if(num == 1)
-//        return 1
-//    else if(num == 2)
-//        return 2
-//    else
-//        return tileRecursion(num - 2) + tileRecursion(num - 1)
-//}
-
+var array = MutableList<Int>(1000001) { -1 }
 
 fun main(args: Array<String>) {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val num = br.readLine().toInt()
-    var array = mutableListOf<Int>()
 
-    array.add(1)
-    array.add(2)
-
-    for(i in 2..num){
-        val tempNum = array[i - 2] + array[i - 1]
-        array.add(tempNum % 15746)
-    }
-
-    println(array[num - 1])
+    println(tileRecursion(num) % 15746)
 }
+
+fun tileRecursion(num: Int): Int {
+    if (array[num] != -1)
+        return array[num]
+
+    if (num == 1)
+        return 1
+    else if (num == 2)
+        return 2
+    else {
+//        return tileRecursion(num - 2) + tileRecursion(num - 1)
+        array[num] = (tileRecursion(num - 2) + tileRecursion(num - 1)) % 15746
+        return array[num]
+    }
+}
+
+//fun main(args: Array<String>) {
+//    val br = BufferedReader(InputStreamReader(System.`in`))
+//    val num = br.readLine().toInt()
+//    var tileArray = mutableListOf<Int>()
+//
+//    tileArray.add(1)
+//    tileArray.add(2)
+//
+//    for (i in 2..num) {
+//        val tempNum = tileArray[i - 2] + tileArray[i - 1]
+//        tileArray.add(tempNum % 15746)
+//    }
+//
+//    println(tileArray[num - 1])
+//}
