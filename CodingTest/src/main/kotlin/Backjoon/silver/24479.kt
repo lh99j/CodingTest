ef0d0a6fb3db3d91b6adbb69ed445af34a0c391f
lh99j <grand2181@gmail.com>
2023-03-07 16:28:44 +0900

    백준 24479(s2) - 깊이 우선 탐색 1

--- CodingTest/src/main/kotlin/Backjoon/silver/24479.kt
+++ CodingTest/src/main/kotlin/Backjoon/silver/24479.kt
@@ -38,3 +38,34 @@ fun dfs(mat: Array<Array<Int>>, r: Int) {
     }
 
 }
+
+//var visited = intArrayOf()
+//var order = 0
+//
+//fun main() {
+//    val inputs = readln().split(" ").map{ it.toInt() }
+//    val pointCount = inputs[0]
+//    val lineCount = inputs[1]
+//    val startPoint = inputs[2] - 1
+//    val lines = Array<MutableList<Int>>(pointCount) { mutableListOf() }
+//    repeat(lineCount) {
+//        readln().split(" ").map { it.toInt() - 1 }.let {
+//            lines[it.first()].add(it.last())
+//            lines[it.last()].add(it.first())
+//        }
+//    }
+//    lines.forEach { it.sort() }
+//
+//    visited = IntArray(pointCount) { order }
+//
+//    dfs(lines, startPoint)
+//
+//    visited.forEach { println(it) }
+//}
+//
+//fun dfs(lines: Array<MutableList<Int>>, startPoint: Int) {
+//    visited[startPoint] = ++order
+//    lines[startPoint].forEach {
+//        if(visited[it] == 0) dfs(lines, it)
+//    }
+//}
