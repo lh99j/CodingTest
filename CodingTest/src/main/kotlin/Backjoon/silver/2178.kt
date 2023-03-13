1fb585dbba7ece24e741465efbb95bf9327ba021
lh99j <grand2181@gmail.com>
2023-03-13 12:22:01 +0900

    백준 2178(s1) - 미로 탐색

--- CodingTest/src/main/kotlin/Backjoon/silver/2178.kt
+++ CodingTest/src/main/kotlin/Backjoon/silver/2178.kt
@@ -3,66 +3,55 @@ import java.util.*
 
 private var dx = intArrayOf(0, -1, 0, 1)
 private var dy = intArrayOf(1, 0, -1, 0)
-private var N = 0
-private var M = 0
+
+private data class Dot(var x: Int, var y: Int)
 
 fun main(args: Array<String>) {
     val br = BufferedReader(InputStreamReader(System.`in`))
     var (n, m) = br.readLine().split(" ").map { it.toInt() }
 //    var maze = Array<MutableList<Int>>(n){ mutableListOf() }
-    N = n
-    M = m
-    var maze = Array(N) { Array(M) { 0 } }
 
-    for (i in 0 until N) {
+    var maze = Array(n) { Array(m) { 0 } }
+
+    for (i in 0 until n) {
         var str = br.readLine().toString()
 
-        for (j in 0 until M) {
+        for (j in 0 until m) {
             maze[i][j] = str[j].digitToInt()
         }
     }
 
 
-    bfs(maze, N, M)
-
-    // 배열 출력
-//    for (i in 0 until maze.size) {
-//        for (j in 0 until maze[0].size) {
-//            print("${maze[i][j]} ")
-//        }
-//        println()
-//    }
+    bfs(maze, n, m)
 
 }
 
-private fun bfs(mat: Array<Array<Int>>, x: Int, y: Int) {
-    var q: Queue<Pair<Int, Int>> = LinkedList<Pair<Int, Int>>()
-    var visited = Array(x) { Array(y) { 0 } }
-    var answer = 0
+private fun bfs(mat: Array<Array<Int>>, x: Int, y: Int){
+    var q: Queue<Dot> = LinkedList<Dot>()
+    var visited = Array(x) { Array(y) { 1 } }
 
     visited[0][0] = 1
-    q.offer(Pair(0, 0))
+    q.add(Dot(0, 0))
 
     while (q.isNotEmpty()) {
         var temp = q.poll()
 
         for (i in 0 until 4) {
-            var tempX = temp.first + dx[i]
-            var tempY = temp.second + dy[i]
+            var tempX = temp.x + dx[i]
+            var tempY = temp.y + dy[i]
 
 
-            if (tempX !in 0 until x || tempY !in 0 until y || mat[tempX][tempY] == 0 || visited[tempX][tempY] != 0) {
+            if (tempX !in 0 until x  || tempY !in 0 until y || mat[tempX][tempY] == 0 || visited[tempX][tempY] != 1)
                 continue
-            }
 
-            visited[tempX][tempX] = 1
-            q.offer(Pair(tempX, tempY))
-            answer++
+            q.add(Dot(tempX, tempY))
+            visited[tempX][tempY] = visited[temp.x][temp.y] + 1
 
             if (tempX == x - 1 && tempY == y - 1) {
-                println(answer)
+                println(visited[tempX][tempY])
                 break
             }
+
         }
     }
 }
