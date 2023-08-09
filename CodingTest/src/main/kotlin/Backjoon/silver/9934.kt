f339fafd243ef5dd6ac6eacb52699df7bdde1747
lh99j <grand2181@gmail.com>
2023-08-09 19:35:46 +0900

    백준 9934(s1) - 완전 이진 트리

--- CodingTest/src/main/kotlin/Backjoon/silver/9934.kt
+++ CodingTest/src/main/kotlin/Backjoon/silver/9934.kt
@@ -8,7 +8,7 @@ fun main(args: Array<String>) {
 
     var tree = Array<MutableList<Int>>(floor){ mutableListOf() }
 
-    mergeAndSort(inorderTraversal, tree, 0, inorderTraversal.size, 0)
+    divideArray(inorderTraversal, tree, 0, inorderTraversal.size, 0)
 
     tree.forEach {
         it.forEach{
@@ -18,13 +18,13 @@ fun main(args: Array<String>) {
     }
 }
 
-private fun mergeAndSort(list: List<Int>, tree: Array<MutableList<Int>>, left: Int, right: Int, count: Int) {
+private fun divideArray(list: List<Int>, tree: Array<MutableList<Int>>, left: Int, right: Int, count: Int) {
     var mid = 0
 
     if (left < right) {
         mid = (left + right) / 2
         tree[count].add(list[mid])
-        mergeAndSort(list, tree, left, mid, count + 1); // 앞쪽 부분 리스트 정렬 -정복(Conquer)
-        mergeAndSort(list, tree, mid + 1, right, count + 1); // 뒤쪽 부분 리스트 정렬 -정복(Conquer)
+        divideArray(list, tree, left, mid, count + 1);
+        divideArray(list, tree, mid + 1, right, count + 1);
     }
 }
