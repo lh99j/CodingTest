495ab9d940cb5b0d2480fd90c5fd34bf1655e60e
lh99j <grand2181@gmail.com>
2023-08-21 13:05:24 +0900

    백준 15900(s1) - 나무 탈출

---
+++ CodingTest/src/main/kotlin/Backjoon/silver/15900.kt
@@ -0,0 +1,44 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+private lateinit var visited: Array<Int>
+private var total = 0
+fun main(args: Array<String>){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+    var tree = Array<MutableList<Int>>(n + 1){ mutableListOf() }
+
+    visited = Array(n + 1){ 0 }
+
+    tree[0].add(0)
+
+    repeat(n - 1){
+        val (node1, node2) = br.readLine().split(" ").map { it.toInt() }
+
+        tree[node1].add(node2)
+        tree[node2].add(node1)
+    }
+
+    dfs(tree, 1, 0)
+
+    if(total % 2 == 0){
+        println("No")
+    }else{
+        println("Yes")
+    }
+}
+
+private fun dfs(tree: Array<MutableList<Int>>, node: Int, cnt: Int){
+    visited[node] = 1
+
+    var isLeaf = true
+
+    tree[node].forEach {
+        if(visited[it] != 1){
+            dfs(tree, it, cnt + 1)
+            isLeaf = false
+        }
+    }
+
+    if(isLeaf) total += cnt
+}
