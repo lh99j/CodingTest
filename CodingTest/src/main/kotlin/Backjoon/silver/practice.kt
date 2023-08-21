86bf283fbf52ed6c4acc2b48e95ebf4977d095ec
lh99j <grand2181@gmail.com>
2023-08-21 16:39:15 +0900

    백준 10798

--- CodingTest/src/main/kotlin/Backjoon/silver/practice.kt
+++ CodingTest/src/main/kotlin/Backjoon/silver/practice.kt
@@ -1,9 +1,32 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.IndexOutOfBoundsException
+import kotlin.math.max
 
 fun main(args: Array<String>) {
-    var answer = ""
-    var str = "1 3 -1 2"
-    var list: List<Int> = str.split(" ").map{ it.toInt() }.toList()
-    
-    answer = "${list.minOrNull()} ${list.minOrNull()}"
-    println(answer)
+    val br = BufferedReader(InputStreamReader(System.`in`))
+
+    var nums = Array<MutableList<String>>(5){ mutableListOf() }
+    var str = ""
+    var maxSize = 0
+
+    for(i in 0 until 5){
+        var str = br.readLine().toString()
+        maxSize = max(maxSize, str.length)
+
+        for(j in str.indices){
+            nums[i].add(str[j].toString())
+        }
+
+    }
+
+    for(i in 0 until maxSize){
+        for(j in 0 until 5){
+            if(nums[j].getOrNull(i) != null){
+                str += nums[j][i]
+            }
+        }
+    }
+
+    println(str)
 }
