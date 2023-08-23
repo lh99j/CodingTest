f276cb59aa804ddd5dabedacf33f4863e1ddc82b
lh99j <grand2181@gmail.com>
2023-08-23 09:19:02 +0900

    백준 15900(s1) - 나무 탈출 bfs, dfs 방법 둘다 추가

--- CodingTest/src/main/kotlin/Backjoon/silver/15900.kt
+++ CodingTest/src/main/kotlin/Backjoon/silver/15900.kt
@@ -1,14 +1,13 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.util.*
+import kotlin.collections.HashSet
 
-private lateinit var visited: Array<Int>
 private var total = 0
 fun main(args: Array<String>){
     val br = BufferedReader(InputStreamReader(System.`in`))
     val n = br.readLine().toInt()
     var tree = Array<MutableList<Int>>(n + 1){ mutableListOf() }
-
-    visited = Array(n + 1){ 0 }
 
     tree[0].add(0)
 
@@ -19,8 +18,9 @@ fun main(args: Array<String>){
         tree[node2].add(node1)
     }
 
-    dfs(tree, 1, 0)
+//    dfs(tree, 1, 0)
 
+    total = bfs(tree, 1)
     if(total % 2 == 0){
         println("No")
     }else{
@@ -43,16 +43,76 @@ fun main(args: Array<String>){
 //    if(isLeaf) total += cnt
 //}
 
-private fun dfs(tree: Array<MutableList<Int>>, node: Int, cnt: Int){
-    visited[node] = 1
+//private fun dfs(tree: Array<MutableList<Int>>, node: Int, cnt: Int){
+//    visited[node] = 1
+//
+//    tree[node].forEach {
+//        if(visited[it] != 1){
+//            dfs(tree, it, cnt + 1)
+//        }
+//    }
+//
+//    if(node != 1 && tree[node].size == 1){
+//        total += cnt
+//    }
+//}
 
-    tree[node].forEach {
-        if(visited[it] != 1){
-            dfs(tree, it, cnt + 1)
+//bfs
+//private fun bfs(tree: Array<MutableList<Int>>, node: Int): Int{
+//    var q: Queue<Int> = LinkedList()
+//    var total = 0
+//
+//    visited[node] = 1
+//    q.offer(node)
+//
+//    while(q.isNotEmpty()){
+//        var popNode = q.poll()
+//
+//        tree[popNode].forEach {
+//            if(visited[it] == 0){
+//                visited[it] = visited[popNode] + 1
+//                q.offer(it)
+//            }
+//
+//            if(tree[it].size == 1){
+//                total += visited[it] - 1
+//            }
+//        }
+//    }
+//
+//    return total
+//}
+
+private fun bfs(tree: Array<MutableList<Int>>, node: Int): Int{
+    var q: Queue<Int> = LinkedList()
+    var visited = HashSet<Int>()
+
+    var total = 0
+    var floor = 1
+
+    visited.add(node)
+    q.offer(node)
+
+    while(q.isNotEmpty()){
+        var size = q.size
+
+        for(i in 0 until size){
+            var temp = q.poll()
+
+            tree[temp].forEach {
+                if(it !in visited){
+                    q.offer(it)
+                    visited.add(it)
+                }
+
+                if(tree[it].size == 1){
+                    total += floor
+                }
+            }
         }
+
+        floor++
     }
 
-    if(node != 1 && tree[node].size == 1){
-        total += cnt
-    }
+    return total
 }
