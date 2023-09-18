88867035798bcc81964227dc3ace85b3980e55d3
lh99j <grand2181@gmail.com>
2023-09-18 14:34:31 +0900

    백준 26260(g5) - 이가 빠진 이진 트리

--- CodingTest/src/main/kotlin/Backjoon/gold/26260.kt
+++ CodingTest/src/main/kotlin/Backjoon/gold/26260.kt
@@ -1,5 +1,4 @@
 import java.io.*
-import java.util.Collections
 import java.util.*
 
 private data class Node26260(val left: Int, val right: Int)
@@ -7,56 +6,37 @@ private data class Node26260(val left: Int, val right: Int)
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     var size = br.readLine().toInt()
-    var nodes = br.readLine().split(" ").map { it.toInt() }
+    var arr = br.readLine().split(" ").map { it.toInt() }
     var x = br.readLine().toInt()
     var tree = mutableListOf<Int>()
-
-    Collections.replaceAll(nodes, -1, x)
-    nodes = nodes.sorted()
+    var nodes = arr.map{ if(it == -1) x else it }.sorted()
 
     tree.add(nodes[nodes.size / 2])
-    makeTreeArr(tree, nodes, 0, nodes.size - 1)
-
-    println(tree.toList().toString())
+    buildTree(tree, nodes, 0, nodes.size - 1)
     postorder(tree, 0)
 }
 
-//private fun makeTreeArr(tree: MutableList<Int>, nodes: List<Int>, left: Int, right: Int) {
-//    var mid = (left + right) / 2
-//
-//    if(left < right && right - left > 1){
-//        tree.add(nodes[(left + mid) / 2])
-//        tree.add(nodes[(mid + 1 + right) / 2])
-//
-//        makeTreeArr(tree, nodes, left, mid)
-//        makeTreeArr(tree, nodes, mid+1, right)
-//    }
-//}
-
-private fun makeTreeArr(tree: MutableList<Int>, nodes: List<Int>, left: Int, right: Int) {
-    var q: Queue<Node26260> = LinkedList()
+private fun buildTree(tree: MutableList<Int>, nodes: List<Int>, left: Int, right: Int) {
+    val q: Queue<Node26260> = LinkedList()
     q.offer(Node26260(left, right))
-    var mid = 0
 
     while (q.isNotEmpty()) {
-        var node = q.poll()
-        var left = node.left
-        var right = node.right
-        mid = (left + right) / 2
+        val node = q.poll()
+        val mid = (node.left + node.right) / 2
 
-        if (left < right && right - left > 1) {
-            tree.add(nodes[(left + mid) / 2])
-            tree.add(nodes[(mid + 1 + right) / 2])
+        if (node.left < node.right && node.right - node.left > 1) {
+            tree.add(nodes[(node.left + mid) / 2])
+            tree.add(nodes[(mid + 1 + node.right) / 2])
 
-            q.offer(Node26260(left, mid))
-            q.offer(Node26260(mid + 1, right))
+            q.offer(Node26260(node.left, mid))
+            q.offer(Node26260(mid + 1, node.right))
         }
     }
 
 }
 
 private fun postorder(tree: MutableList<Int>, node: Int) {
-    if (node in 0 until tree.size) {
+    if (node in tree.indices) {
         postorder(tree, 2 * node + 1)
         postorder(tree, 2 * node + 2)
         print("${tree[node]} ")
