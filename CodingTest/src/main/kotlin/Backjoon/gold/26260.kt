df379238da17090d35ed724befa2f9034735a8d3
lh99j <grand2181@gmail.com>
2023-09-18 15:12:35 +0900

    백준 26260(g5) - 이가 빠진 이진 트리

--- CodingTest/src/main/kotlin/Backjoon/gold/26260.kt
+++ CodingTest/src/main/kotlin/Backjoon/gold/26260.kt
@@ -9,36 +9,18 @@ fun main() {
     var arr = br.readLine().split(" ").map { it.toInt() }
     var x = br.readLine().toInt()
     var tree = mutableListOf<Int>()
-    var nodes = arr.map{ if(it == -1) x else it }.sorted()
+    var nodes = arr.map { if (it == -1) x else it }.sorted()
 
     tree.add(nodes[nodes.size / 2])
-    buildTree(tree, nodes, 0, nodes.size - 1)
-    postorder(tree, 0)
+    postorder(nodes, 0, nodes.size)
 }
 
-private fun buildTree(tree: MutableList<Int>, nodes: List<Int>, left: Int, right: Int) {
-    val q: Queue<Node26260> = LinkedList()
-    q.offer(Node26260(left, right))
+private fun postorder(nodes: List<Int>, left: Int, right: Int) {
+    val mid = (left + right) / 2
 
-    while (q.isNotEmpty()) {
-        val node = q.poll()
-        val mid = (node.left + node.right) / 2
-
-        if (node.left < node.right && node.right - node.left > 1) {
-            tree.add(nodes[(node.left + mid) / 2])
-            tree.add(nodes[(mid + 1 + node.right) / 2])
-
-            q.offer(Node26260(node.left, mid))
-            q.offer(Node26260(mid + 1, node.right))
-        }
-    }
-
-}
-
-private fun postorder(tree: MutableList<Int>, node: Int) {
-    if (node in tree.indices) {
-        postorder(tree, 2 * node + 1)
-        postorder(tree, 2 * node + 2)
-        print("${tree[node]} ")
+    if (left < right) {
+        postorder(nodes, left, mid)
+        postorder(nodes, mid + 1, right)
+        print("${nodes[mid]} ")
     }
 }
