f608351f629a6eae36826bf84011cc6f7deb861f
lh99j <grand2181@gmail.com>
2023-10-11 16:36:46 +0900

    프로그래머스 level3 - 베스트앨범(해시)

--- CodingTest/src/main/kotlin/Programmers/level3/BestAlbum.kt
+++ CodingTest/src/main/kotlin/Programmers/level3/BestAlbum.kt
@@ -8,6 +8,7 @@ class BestAlbum {
             album[genres[i]] = album.getOrDefault(genres[i], 0) + plays[i]
         }
 
+        //앨범을 value값으로 내림차순 정렬
         var sortAlbum = album.toList().sortedByDescending { it.second }.toMap().toMutableMap()
 
         sortAlbum.onEachIndexed { index, entry ->
@@ -20,6 +21,7 @@ class BestAlbum {
                 }
             }
 
+            //sortAlbum 즉, 가장 재생수가 많은 앨범부터 (원래 genres 배열의 인덱스값, 재생수)를 담아 재생수 기준으로 내림차순 정렬
             var sortGenreMap = genreMap.toList().sortedByDescending { it.second }.toMap().toMutableMap()
 
             var cnt = 0
@@ -28,6 +30,8 @@ class BestAlbum {
                 if(cnt < 2){
                     var (idx, value) = entry.toString().split("=")
                     ana.add(idx.toInt())
+
+                    //카운트를 넣어주어 2보다 작을때만 실행됨
                     cnt++
                 }
             }
