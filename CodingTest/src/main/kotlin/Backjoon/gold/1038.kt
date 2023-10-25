03170e1a04ac5156390fa42e602dd0de061241c6
lh99j <grand2181@gmail.com>
2023-10-25 10:33:17 +0900

    백준 1038(g5) - 감소하는 수

--- CodingTest/src/main/kotlin/Backjoon/gold/1038.kt
+++ CodingTest/src/main/kotlin/Backjoon/gold/1038.kt
@@ -4,15 +4,15 @@ import java.lang.StringBuilder
 import kotlin.system.exitProcess
 
 private var cnt = -1
-private var visited = Array(10){ false }
+private var visited = Array(10) { false }
 private var size = 0
 private lateinit var arr: Array<Int>
 
-fun main()= with(BufferedReader(InputStreamReader(System.`in`))){
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     size = readLine().toInt()
 
-    for(i in 1..10){
-        arr = Array(i){ 0 }
+    for (i in 1..10) {
+        arr = Array(i) { 0 }
         back(0, i)
     }
 
@@ -20,14 +20,14 @@ fun main()= with(BufferedReader(InputStreamReader(System.`in`))){
 
 }
 
-private fun back(depth: Int, c: Int){
-    if(depth == c){
+private fun back(depth: Int, c: Int) {
+    if (depth == c) {
 
-        if(check(arr)){
+        if (check(arr)) {
             cnt++
         }
 
-        if(cnt == size){
+        if (cnt == size) {
             val sb = StringBuilder()
 
             arr.forEach {
@@ -40,8 +40,8 @@ private fun back(depth: Int, c: Int){
         return
     }
 
-    for(i in 0 until 10){
-        if(visited[i])
+    for (i in 0 until 10) {
+        if (visited[i])
             continue
 
         visited[i] = true
@@ -52,14 +52,14 @@ private fun back(depth: Int, c: Int){
     }
 }
 
-private fun check(nums: Array<Int>): Boolean{
+private fun check(nums: Array<Int>): Boolean {
     var max = nums[0]
 
-    if(nums.size > 1){
-        for(i in 1 until nums.size){
-            if(max < nums[i]){
+    if (nums.size > 1) {
+        for (i in 1 until nums.size) {
+            if (max < nums[i]) {
                 return false
-            }else{
+            } else {
                 max = nums[i]
             }
         }
