7a5d5c491e60a417f7b0f88e8808530969cd4e22
lh99j <grand2181@gmail.com>
2023-12-13 12:53:31 +0900

    프로그래머스 level2 - JadenCase 문자열 만들기(연습문제)

--- CodingTest/src/main/kotlin/Programmers/level2/JadenCase.kt
+++ CodingTest/src/main/kotlin/Programmers/level2/JadenCase.kt
@@ -1,19 +1,28 @@
 class JadenCase {
     fun solution(s: String): String {
-        var answer = ""
-        var list: List<String> = s.split(" ").toList()
+        var strings = s.split(" ")
+        var new = mutableListOf<String>()
 
-        for(i in list.indices){
-            val str = list[i]
-            if(str[0].digitToInt() in 97..122){
-                str[0].uppercaseChar()
+        for (i in strings.indices) {
+            val str = strings[i]
+            if (str.isNotEmpty()) {
+                var newS = ""
+                newS += str[0].uppercaseChar()
+                if (str.length > 1) {
+                    newS += str.substring(1, str.length).lowercase()
+                }
+                new.add(newS)
+            }else {
+                new.add(str)
             }
         }
 
-        return answer
+
+        return new.joinToString(" ")
     }
 }
 
 fun main(args: Array<String>) {
-
+    val jc = JadenCase()
+    println(jc.solution("  for the what 1what  "))
 }
