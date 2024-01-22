653fcc46b5075450587787259f22567e8b14e777
lh99j <grand2181@gmail.com>
2024-01-22 13:56:24 +0900

    백준 1374(g5) - 강의실

---
+++ CodingTest/src/main/kotlin/Backjoon/gold/1374.kt
@@ -0,0 +1,29 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.PriorityQueue
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val cnt = br.readLine().toInt()
+    var tL = mutableListOf<Pair<Int, Int>>()
+    var pQ = PriorityQueue<Int>()
+
+    repeat(cnt) {
+        val (num, x, y) = br.readLine().split(" ").map { it.toInt() }
+        tL.add(x to y)
+    }
+
+    tL.sortedWith(compareBy({ it.first }, { it.second })).forEach {
+        if(pQ.isEmpty()){
+            pQ.offer(it.second)
+        }else{
+            val p = pQ.peek()
+            if(p <= it.first){
+                pQ.poll()
+            }
+            pQ.offer(it.second)
+        }
+    }
+
+    println(pQ.size)
+}
