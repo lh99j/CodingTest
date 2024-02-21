16dd3ea91850292b67149661a9b423fd7b2a5e8f
lh99j <grand2181@gmail.com>
2024-02-21 12:35:13 +0900

    백준 1043(G4) - 거짓말

--- CodingTest/src/main/kotlin/Backjoon/gold/1043.kt
+++ CodingTest/src/main/kotlin/Backjoon/gold/1043.kt
@@ -7,22 +7,22 @@ fun main() {
     val truthPeoples = br.readLine().split(" ").map { it.toInt() }
 
     val truths = HashSet<Int>()
-    val liers = HashSet<Int>()
     var inputs: MutableList<List<Int>> = mutableListOf()
 
+    // 진실을 아는 사람의 입력이 1보다 클 때만 진실 배열에 추가
     if (truthPeoples.size > 1) {
         for (i in 1 until truthPeoples.size) {
             truths.add(truthPeoples[i])
         }
     }
 
-
+    // 입력을 담고 있는 배열 추가
     repeat(partySize) {
         val peoples = br.readLine().split(" ").map { it.toInt() }
         inputs.add(peoples)
     }
 
-    var validInputs = inputs.toMutableList()
+    var validIndex = HashSet<Int>()
 
     while (true) {
         var end = true
@@ -32,32 +32,32 @@ fun main() {
         }
 
         for (i in 0 until inputs.size) {
-            var knowTruth = false
+            if (i !in validIndex) {
+                var knowTruth = false
 
-            //만약 진실을 아는사람이 하나라도 있다면
-            for (j in 1 until inputs[i].size) {
-                if (inputs[i][j] in truths) {
-                    knowTruth = true
+                //만약 진실을 아는사람이 하나라도 있다면
+                for (j in 1 until inputs[i].size) {
+                    if (inputs[i][j] in truths) {
+                        knowTruth = true
+                        break
+                    }
+                }
+
+                //진실을 아는사람이 파티에 있었다면
+                if (knowTruth) {
+
+                    // 해당 파티에 온 사람들은 전부 진실 리스트에 추가
+                    for (j in 1 until inputs[i].size) {
+                        truths.add(inputs[i][j])
+                    }
+
+                    //해당 파티에서는 진실을 말할 수 없다.
+                    validIndex.add(i)
+                    end = false
                     break
                 }
             }
-
-            //진실을 아는사람이 파티에 있었다면
-            if (knowTruth) {
-
-                // 해당 파티에 온 사람들은 전부 진실 리스트에 추가
-                for (j in 1 until inputs[i].size) {
-                    truths.add(inputs[i][j])
-                }
-
-                //해당 파티에서는 진실을 말할 수 없다.
-                validInputs.remove(inputs[i])
-                end = false
-                break
-            }
         }
-
-        inputs = validInputs.toMutableList()
 
         if (end) {
             break
@@ -65,5 +65,5 @@ fun main() {
 
     }
 
-    println(inputs.size)
+    println(inputs.size - validIndex.size)
 }
