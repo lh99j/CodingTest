13dcc58f0dcd175bf09fe248ca9cb4686fa6aa5d
lh99j <grand2181@gmail.com>
2024-06-20 12:00:25 +0900

    프로그래머스 level2 - 2개 이하로 다른 비트 (월간 코드 챌린지 시즌2)

--- CodingTest/src/main/kotlin/Programmers/level2/DifferentBit.kt
+++ CodingTest/src/main/kotlin/Programmers/level2/DifferentBit.kt
@@ -7,41 +7,17 @@ class DifferentBit {
                 ans.add(1L)
                 continue
             }
+
             var binary = numbers[i].toString(2)
-            if(binary[binary.length - 1] == '0'){
-                binary = binary.substring(0, binary.length - 1) + "1"
-            }else{
-                var indexFirst1 = -1
+            val length = binary.length
 
-                for(i in binary.indices){
-                    if(binary[i] == '1'){
-                        indexFirst1 = i
-                        break
-                    }
-                }
-
-                var index0 = -1
-
-                for(i in indexFirst1 until binary.length){
-                    if(binary[i] == '0'){
-                        index0 = i
-                    }
-                }
-
-                if(index0 == -1){
-                    binary = "10" + binary.substring(1, binary.length)
-                }else{
-                    var search1 = -1
-
-                    for(i in index0 until binary.length){
-                        if(binary[i] == '1'){
-                            search1 = i
-                            break
-                        }
-                    }
-
-                    binary = binary.substring(0, index0) + "10" + binary.substring(search1 + 1, binary.length)
-                }
+            if(binary[length - 1] == '0'){
+                binary = binary.substring(0, length - 1) + "1"
+            }else if(binary.contains('0')){
+                val idx = binary.lastIndexOf('0')
+                binary = binary.replaceRange(idx, idx + 1, "10")
+            } else{
+                binary = "10" + binary.substring(1, length)
             }
 
             ans.add(binary.toLong(2))
